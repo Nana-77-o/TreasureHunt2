2982a11afad1cbf27b535108d6f797d6ee5e2173
Nana_77 <nana77amery@gmail.com>
2021-08-30 23:08:57 +0900

    主音量、効果音を追加して宝箱をゲットしたら音が鳴るようにした

--- Assets/C#/Count.cs
+++ Assets/C#/Count.cs
@@ -8,11 +8,18 @@ public class Count : MonoBehaviour
 {
     private int count = 0;
     public Text TreasureCount;
-    
+    public AudioClip sound1;
+    AudioSource audioSoure;
+
+    private void Start()
+    {
+        audioSoure = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Treasure")
         {
+            audioSoure.PlayOneShot(sound1);
             count++;
             TreasureCount.text =+ count +"/5" ;
             Destroy(collision.gameObject);
--- Assets/C#/CountP.cs
+++ Assets/C#/CountP.cs
@@ -8,10 +8,18 @@ public class CountP : MonoBehaviour
 {
     int count; 
     public Text TreasureCount;
+    public AudioClip sound1;
+    AudioSource audioSoure;
+
+    private void Start()
+    {
+        audioSoure = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Treasure")
         {
+            audioSoure.PlayOneShot(sound1);
             count++;
             TreasureCount.text = +count + "/10";
             Destroy(collision.gameObject);
