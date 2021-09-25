abdec3030aed0a10bd4b041369ee9b454bddf0b8
Nana_77 <nana77amery@gmail.com>
2021-09-25 10:57:43 +0900

    偽宝箱の範囲と個数を難易度に合わせてスクリプトを作った

---
+++ Assets/C#/Randam/RandamHard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandamHard : MonoBehaviour
+{
+	//少なくて広い
+	public GameObject cube;
+	public GameObject Treasyre2;
+	void Start()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			//オブジェクトの座標
+			float x = Random.Range(-499.0f, 499.0f);
+			float z = Random.Range(-499.0f, 499.0f);
+			float y = 7.0f;
+
+			//オブジェクトを生産
+			Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+		}
+		for (int i = 0; i < 5; i++)
+		{
+			//オブジェクトの座標
+			float x = Random.Range(-499.0f, 100.0f);
+			float z = Random.Range(-499.0f, 100.0f);
+			float y = 7.0f;
+
+			//オブジェクトを生産
+			Instantiate(Treasyre2, new Vector3(x, y, z), Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/C#/Randam/RandamNomal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandamNomal : MonoBehaviour
+{
+	public GameObject cube;
+	public GameObject Treasyre2;
+
+	void Start()
+	{
+		//多くて狭い
+		for (int i = 0; i < 10; i++)
+		{
+			//オブジェクトの座標
+			float x = Random.Range(-100.0f, 100.0f);
+			float z = Random.Range(-100.0f, 100.0f);
+			float y = 7.0f;
+
+			//オブジェクトを生産
+			Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			//オブジェクトの座標
+			float x = Random.Range(-100.0f, 100.0f);
+			float z = Random.Range(-100.0f, 100.0f);
+			float y = 7.0f;
+
+			//オブジェクトを生産
+			Instantiate(Treasyre2, new Vector3(x, y, z), Quaternion.identity);
+		}
+	}
+}
